831d4227374c2d7f1426b6f7dd083a3955c86fab
Tony <chiangloveslynn@gmail.com>
2016-03-25 00:03:03 +0800

    some real easy bones modify.

--- header.php
+++ header.php
@@ -5,13 +5,17 @@
  */
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<?php wp_head(); ?>
+	</head>
 
-<?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
+	<body <?php body_class(); ?>>
+		<header>
+			<nav>
+			<?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+			</nav>
+		</header>

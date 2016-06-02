90929a4e61886ae75900c2f9e35890d79d99613c
Tony <chiangloveslynn@gmail.com>
2016-06-02 23:00:47 +0800

    init

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,7 @@
  */
 
 var project              = 'WPGulpTheme'; // Project Name.
-var projectURL           = 'src.wordpress-develop.dev'; // Project URL. Could be something like localhost:8888.
+var projectURL           = 'jade.dev'; // Project URL. Could be something like localhost:8888.
 var productURL           = './'; // Theme/Plugin URL. Leave it like it is, since our gulpfile.js lives in the root folder.
 
 
